3b441250337fee90165f41a1e6e2adc41c6a31eb
Antonio Ladeia <contato@antonioladeia.com>
2019-08-17 17:57:37 -0300

    create a generic card repository

---
+++ src/main/java/com/antonioladeia/prepag/repository/CardRepository.java
@@ -0,0 +1,8 @@
+package com.antonioladeia.prepag.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.antonioladeia.prepag.models.Card;
+
+public interface CardRepository extends JpaRepository<Card, Long>{
+
+}
